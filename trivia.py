046822bc2672643cb51dd7bd8b43c17561133101
psudhakar <sudhakar.parsi@gmail.com>
2023-12-06 15:25:11 -0600

    Update trivia.py

--- trivia.py
+++ trivia.py
@@ -63,6 +63,7 @@ st.set_page_config(page_title="Trivia Night by NCHS After Prom Team", layout="wi
     'About': None
 })
 
+
 st.markdown("""
     <style>
     .block-container {
@@ -77,7 +78,7 @@ st.markdown("""
     }
     
     h5 {
-        color: blue;
+        color: Purple;
         text-align: center;
     }
     .font {
@@ -96,27 +97,77 @@ st.markdown("# Trivia Night 2024 ")
 st.markdown("##### Organized by NCHS After Prom Parents Committee 2024")
 st.markdown("##### Sweat the Questions, Sweeten the Charity Pot: Trivia Night for a Good Cause!")
 
-# Event details
-st.header("Join Us!")
-st.markdown("""
-- 🗓️ **Date:** Saturday, February 3rd, 2024
-- 🕗 **Time:** 6 PM to 9 PM
-- 📍 **Venue:** [Insert Venue Here]
-- 💡 Hosted by the legendary Trivia City!
-""")
+col1, col2 = st.columns(2)
 
-# Registration instructions
-st.header("Registration")
-st.markdown("""
-    1.	Gather your cleverest crew and register your team below!
-        Squad Discount: Only \$100 for a table of 6, otherwise $20 per individual
-    2.	Flying solo, no problem! We are Trivia matchmakers, ready to pair you with your trivia soulmates!
-    3.	Fuel your brains with delicious snacks and drinks (available for purchase)!
-    4.	Put your heads together to conquer tricky questions and earn bragging rights!
-    5.	Most importantly, support a fantastic cause: creating a safe and memorable Afterprom experience for our NCHS students!
-            
-""", unsafe_allow_html=True)
+with col1:
+    # Event details
+    st.header("Join Us!")
+    st.markdown("""
+    - 🗓️ **Date:** Saturday, February 3rd, 2024
+    - 🕗 **Time:** 6 PM to 9 PM
+    - 📍 **Venue:** Parke Residency, 1413, Leslie Dr, Bloomington, IL
+    - 💡 Hosted by the legendary Trivia City!
+    """)
 
+    # Registration instructions
+    st.header("Registration")
+    st.markdown("""
+        1.	Gather your cleverest crew and register your team below!
+            Squad Discount: Only \$100 for a table of 6, otherwise $20 per individual
+        2.	Flying solo, no problem! We are Trivia matchmakers, ready to pair you with your trivia soulmates!
+        3.	Fuel your brains with delicious snacks and drinks (available for purchase)!
+        4.	Put your heads together to conquer tricky questions and earn bragging rights!
+        5.	Most importantly, support a fantastic cause: creating a safe and memorable Afterprom experience for our NCHS students!
+                
+    """, unsafe_allow_html=True)
+
+    st.markdown("""
+        Reserve your table by filling the form below. Payments can be made by check, or using any payment methods below:
+    """, unsafe_allow_html=True)
+
+    # Form for registration
+    with st.form(key='registration_form'):
+        st.markdown("#### Team Registration - NCHS After Prom 2024 Trivia Night")
+        team_contact = st.text_input("Team Contact:")
+        team_name = st.text_input("Team Name:")
+        team_contact_email = st.text_input("Email address for Team Contact:")
+        team_contact_phone = st.text_input("Phone number for Team Contact:")
+        
+        # Submit button
+        submit_button = st.form_submit_button(label='Submit')
+
+    if submit_button:
+        # Prepare the data to be sent via email
+        data = f"""
+        Team Contact: {team_contact}
+        Team Name: {team_name}
+        Team Contact Email: {team_contact_email}
+        Team Contact Phone: {team_contact_phone}
+        """
+        data += "Use any payment methods below. Please add your name or email address in the comments section of the payment. \n"
+        data += "Using Venmo, pay to  : Jayshri-Patel-5 \n"
+        data += "Using Zelle/Paypal, pay to  : sudhakar.parsi@gmail.com \n"
+        data += "Using Cash, pay to  : $SudhakarParsi \n\n"
+        data += "Once the payment is made, you will receive an email with Krispy Kreme gift voucher in next 24-28 hours. \n"
+        data += "For any questions, please contact nchsjr.board@gmail.com or call (404)800-3312] \n"
+        data += "Make checks payable to 'NCHS After Prom' and mail to 5018 Londonderry Road, Bloomington, IL - 61705"
+
+        # Send the email
+        send_email(team_contact_email, data)
+
+        #Save the sheet to Google Sheets
+
+        registration_data = [team_contact, team_name, team_contact_email, team_contact_phone]
+        save_to_sheet(registration_data, 'https://docs.google.com/spreadsheets/d/1dGjZj-QNGjpn-oGTkeYKuHfw-okNSM6iYh-HLEN255A/edit?usp=sharing')
+
+
+        st.success("Registration submitted successfully! You will receive a confirmation email shortly.")
+
+with col2:
+    #st.image("https://github.com/psudhakar/nchstrivia/blob/main/trivia2.JPG?raw=true", use_column_width=None)
+    st.markdown(f"""<div style="text-align: center;">
+    <img src="https://github.com/psudhakar/nchstrivia/blob/main/trivia3.JPG?raw=true" alt="Image" >
+    </div>""", unsafe_allow_html=True)
 
 # Streamlit app layout
 #st.set_page_config(page_title='Trivia Night Registration', layout='centered')
@@ -124,51 +175,7 @@ st.markdown("""
 #logo_url = "https://drive.google.com/uc?id=1SfAQ50kW6gybO-Pqcs6mFWQ2DiLY0mcH"
 
 
-st.markdown("""
-    **Registration:**
-    Reserve your table by filling the form below. Payments can be made by check, or using any payment methods below:
-""", unsafe_allow_html=True)
-venmourl = "https://venmo.com/u/Jayshri-Patel-5"
-recipient_id = 'Jayshri-Patel-5'
-cash_app_link = f'https://cash.app/$${recipient_id}'
-
-# Form for registration
-with st.form(key='registration_form'):
-    st.markdown("#### Team Registration - NCHS After Prom 2024 Trivia Night")
-    team_contact = st.text_input("Team Contact:")
-    team_name = st.text_input("Team Name:")
-    team_contact_email = st.text_input("Email address for Team Contact:")
-    team_contact_phone = st.text_input("Phone number for Team Contact:")
     
-    # Submit button
-    submit_button = st.form_submit_button(label='Submit')
-
-if submit_button:
-    # Prepare the data to be sent via email
-    data = f"""
-    Team Contact: {team_contact}
-    Team Name: {team_name}
-    Team Contact Email: {team_contact_email}
-    Team Contact Phone: {team_contact_phone}
-    """
-    data += "Use any payment methods below. Please add your name or email address in the comments section of the payment. \n"
-    data += "Using Venmo, pay to  : Jayshri-Patel-5 \n"
-    data += "Using Zelle/Paypal, pay to  : sudhakar.parsi@gmail.com \n"
-    data += "Using Cash, pay to  : $SudhakarParsi \n\n"
-    data += "Once the payment is made, you will receive an email with Krispy Kreme gift voucher in next 24-28 hours. \n"
-    data += "For any questions, please contact nchsjr.board@gmail.com or call (404)800-3312] \n"
-    data += "Make checks payable to 'NCHS After Prom' and mail to 5018 Londonderry Road, Bloomington, IL - 61705"
-
-    # Send the email
-    send_email(team_contact_email, data)
-
-    #Save the sheet to Google Sheets
-
-    registration_data = [team_contact, team_name, team_contact_email, team_contact_phone]
-    save_to_sheet(registration_data, 'https://docs.google.com/spreadsheets/d/1dGjZj-QNGjpn-oGTkeYKuHfw-okNSM6iYh-HLEN255A/edit?usp=sharing')
-
-
-    st.success("Registration submitted successfully! You will receive a confirmation email shortly.")
 
 st.header("Payment Options")
 
@@ -176,14 +183,16 @@ st.header("Payment Options")
 col1, col2, col3, col4 = st.columns(4)
 with col1:
     st.markdown("#### Venmo")
-    st.markdown("Click [here](https://venmo.com/Jayshri-Patel-5) or send it to `Jayshri-Patel-5`")
+    st.markdown("Click [here](https://venmo.com/Li-Boyer) or send it to `Li-Boyer`")
+    st.image("https://github.com/psudhakar/nchstrivia/blob/main/Venmo-Boyer.jpg?raw=true", use_column_width=None)
 with col2:
     st.markdown("#### Zelle/PayPal")
-    st.markdown("Send your payments to: `sudhakar.parsi@gmail.com`")
+    st.markdown("Send your payments to: `Carriezhengli@gmail.com`")
+    st.image("https://github.com/psudhakar/nchstrivia/blob/main/Zelle-Boyer.jpg?raw=true", use_column_width=None)
 with col3:
-    st.markdown("#### Cash App")
-    st.markdown("Click [here](https://cash.app/$SudhakarParsi) or send it to `$SudhakarParsi`")
+    st.markdown("#### Paypal")
+    st.markdown("Send your payments to to `Carriezhengli@gmail.com`")
+    st.image("https://github.com/psudhakar/nchstrivia/blob/main/Paypal-Boyer.jpg?raw=true", use_column_width=None)
 with col4:
     st.markdown("#### Check")
     st.markdown("Make checks payable to 'NCHS After Prom' and mail to `5018 Londonderry Road Bloomington IL - 61705`")
-    
